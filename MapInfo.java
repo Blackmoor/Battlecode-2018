088805f224e98f21432ea25dc579f5d8ce62bbe5
Daryl Tose <daz@blackmoor.org.uk>
2018-01-25 18:16:05 +0000

    Cosmetics

--- MapInfo.java
+++ MapInfo.java
@@ -3,21 +3,22 @@ import java.util.LinkedList;
 import bc.MapLocation;
 
 public class MapInfo {
-		public LinkedList<MapLocation>	neighbours;
-    	public LinkedList<MapLocation>	passableNeighbours;
-    	public LinkedList<MapLocation>	within100;
-    	public LinkedList<MapLocation>	within70;
-    	public LinkedList<MapLocation>	within50;
-    	public LinkedList<MapLocation>	within50_10;
-    	public LinkedList<MapLocation>	within30;
-    	public LinkedList<MapLocation>	within30_8;
-    	public MapLocation 				here;
-    	public boolean					passable;
-    	
-    	public MapInfo(MapLocation mapLocation, boolean p) {
-    		here = mapLocation;
-    		passable = p;
-    		neighbours = null;
-    		passableNeighbours = null;
-    	}
-    }
+	public MapLocation 				here;
+	public boolean					passable;
+	
+	public LinkedList<MapLocation>	neighbours;
+	public LinkedList<MapLocation>	passableNeighbours;
+	public LinkedList<MapLocation>	within100;
+	public LinkedList<MapLocation>	within70;
+	public LinkedList<MapLocation>	within50;
+	public LinkedList<MapLocation>	within50_10;
+	public LinkedList<MapLocation>	within30;
+	public LinkedList<MapLocation>	within30_8;
+	
+	public MapInfo(MapLocation mapLocation, boolean p) {
+		here = mapLocation;
+		passable = p;
+		neighbours = null;
+		passableNeighbours = null;
+	}
+}
